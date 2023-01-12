b06c780898a1c61de3c1925d6e9c7e0ca416a766
egahqusayf <egahqusayf.d@gmail.com>
2023-01-13 01:37:36 +0700

    Memindahkan ctleft ke main js untuk menggunakan vue

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 
-
+const ctleft = [
+    `<div class="w-full flex justify-center items-center h-16 bg-gradient-to-l from-slate-600 via-green-700 to-slate-500 rounded-2xl mb-2">
+        <p class="font-teko text-2xl text-slate-200 tracking-widest ">Awal mula Surabaya Dev</p>
+    </div>`
+   
+];
 
 const app = Vue.createApp({
     data(){
         return({
-            
+            ctleft
         })
     }
 });
 
-app.mount('#app')
+app.mount('#app');
 
 $(document).ready(function(){
 
